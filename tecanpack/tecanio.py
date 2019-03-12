4ec2deed529fd1be8c69e121d2414cdb956bfc9e
Kirk Vander Meulen <kirk.vandermeulen@gmail.com>
2019-03-12 10:48:40 -0500

    class-based update next update continued. TecanPlate wells are now TecanWell with dataclass WellReading as an attribute. WellReading is a single dataclass, no more inheritance

--- tecanpack/tecanio.py
+++ tecanpack/tecanio.py
@@ -5,81 +5,67 @@ from dataclasses import dataclass
 
 @dataclass
 class WellReading:
-    '''Base class for 96-well plate reading'''
+    '''96-well plate reading'''
     measurement: float = None
     expdate: datetime.date = None
     expfname: str = None
     expsheet: str = None
-    volume: float = None
+    msrvolume: float = None
     detection: str = None #dns, bca, etc
     wellid: str = None #A1-H12
-    predvlp_dlnfactor: float = 1.0
-    postdvlp_dlnfactor: float = 1.0
+    predvlp_dlnfactor: float = None
+    postdvlp_dlnfactor: float = None
     experimenter: str = None
     absorbance_wl: int = None #3/5/19---add these as defaults according to dns/bca/etc
     excitation_wl: int = None #3/5/19---add these as defaults according to dns/bca/etc
-    src_vesselid: str = None #reflects whether this measurement is unique to a source vessel
-                             #this will by default get a new uuid.uuid4().hex
-                             #somehow incorporate multiple reads from same well intelligence
+    plateid: str = None #reflects whether this measurement is unique to a source vessel
 #somehow incorporate batches/freshness?
-#add
-@dataclass
-class WellRxnReading(WellReading):
-    '''Base class for well involving reaction or incubation.
-    Overrides WellReading and adds rxn and solultion details'''
+    '''Settings for reaction/incubation. All about the setup pre-developing''' 
     rxntime: float = None
     rxntime_units: str = None
     rxnvessel: str = None
+    rxnvesselid: str = None #this will by default get a new uuid.uuid4().hex
     rxnvessel_wellid: str = None #in case different from wellid of reading
     incubator: str = None
     rxntemp: float = None
     rxnph: float = None
     rxnvol: float = None
     rxnvol_units: str = None
+    rxn_description_string: str = None
     buffername: str = None
     bufferconc: float = None
     bufferconc_units: str = None
-    solutionstr: str = None
+    solution_description_string: str = None
     biorepid: str = None #uuid (hex-rep) reflecting uniquely prepared biosamples
-
-@dataclass
-class WellEnzymeSubstrateReading(WellRxnReading):
-    '''Enzyme-Substrate reaction.
-    Overrides WellRxnReading, adding enzyme and substrate names & concs'''
-    ename: str = None
-    econc: float = None
-    econc_units: str = None
+    #substrate settings
     sname: str = None
     sconc: float = None
     sconc_units: str = None
-    etype: str = None #'cellfree","purified"
-    ename_type: str = "acc" #default is that the name=accession code
-
-@dataclass
-class WellEnzymeControl(WellRxnReading):
-    '''Enzyme-only control well.
-    Overrides WellRxnReading, adding enzyme name & conc'''
+    sbarcode: str = None
+    sprepdate: datetime.date = None
+    sstock_conc: float = None
+    sstock_conc_units: str = None
+    s_description_string: str = None
+    #enzyme settings
     ename: str = None
     econc: float = None
     econc_units: str = None
-    etype: str = None #'cellfree","purified"
-    ename_type: str = "acc" #default is that the name=accession code
-
-@dataclass
-class WellSubstrateControl(WellRxnReading):
-    '''Substrate-only control well.
-    Overrides WellRxnReading, adding substrate name & conc'''
-    sname: str = None
-    sconc: float = None
-    sconc_units: str = None
-
-@dataclass
-class WellStandard(WellRxnReading):
-    '''Standard well for calibration of some reaction.
-    Overrides WellRxnReading, adding standard name & conc'''
-    standardname: str = None
-    standardconc: float = None
-    standardconc_units: float = None
+    ebarcode: str = None
+    eprepdate: datetime.date = None
+    epreptype: str = None #'cellfree","purified"
+    enametype: str = "acc" #default is that the name=accession code
+    evariant: str = None #ko, ea, +cbmx2,etc
+    ethawdate: datetime.date = None
+    estock_conc: float = None
+    estock_conc_units: str = None
+    e_description_string: str = None
+    #standard settings
+    standard_name: str = None
+    standard_conc: float = None
+    standard_conc_units: str = None
+    standard_stock_conc: float = None
+    standard_stock_conc_units: float = None
+    standard_description_string: str = None
 
 def read_tecan_excel(ifpath,shname,plate_header_row=23,plate_header_col="A"):
     skiprows=list(range(plate_header_row-1))
@@ -95,9 +81,43 @@ def read_tecan_excel(ifpath,shname,plate_header_row=23,plate_header_col="A"):
     return platedf
 
 class TecanWell:
-    def __init__(self,wellreading):
-        self.wellreading=wellreading
+    def __init__(self,well_settings_dict,use_experimenter_defaults=True):
+        #can add some error handling here...
+        self.well_settings_dict=well_settings_dict
+        if use_experimenter_defaults:
+            self.set_experimenter_defaults()
+        self.wellreading=WellReading(**{x:well_settings_dict[x] for x in well_settings_dict.keys()})
+           #now determine what type of WellReading subclass this is based on keys in well_dict
+    def set_experimenter_defaults(self):
+        #this could be redone with a dictionary much nicer...
+        assert (self.well_settings_dict['experimenter'] in ['KIRK VANDER MEULEN','ERIC HENNEMAN']),\
+                f"experimenter {self.well_settings_dict['experimenter']} has no default settings"
+        if 'DNS'==self.well_settings_dict['detection'].upper():
+            if 'absorbance_wl' not in self.well_settings_dict.keys():
+                self.well_settings_dict['absorbance_wl']=540
+            if 'predvlp_dlnfactor' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['predvlp_dlnfactor']=1/3
+            if 'postdvlp_dlnfactor' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['postdvlp_dlnfactor']=36/196
+            if 'msrvolume' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['msrvolume']=196
+        elif 'BCA'==self.well_settings_dict['detection'].upper():
+            if 'absorbance_wl' not in self.well_settings_dict.keys():
+                self.well_settings_dict['absorbance_wl']=562
+            if 'predvlp_dlnfactor' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['predvlp_dlnfactor']=5/105
+            if 'postdvlp_dlnfactor' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['postdvlp_dlnfactor']=1
+            if 'msrvolume' not in self.well_settings_dict.keys():
+                if self.well_settings_dict['experimenter'].upper() in ['KIRK VANDER MEULEN','ERIC HENNEMAN']:
+                    self.well_settings_dict['msrvolume']=90
 
+ 
 class TecanPlate:
     def __init__(self,defaultsdict):
         """
@@ -109,10 +129,46 @@ class TecanPlate:
                        'standardconc_units':'mM','standardname':'glucose'}
         Arguments:
         defaultsdict: various fields relevant to WellReadings and subclasses that apply to all wells
+        """
+       #wellids=[x+y for x in ['A','B','C','D','E','F','G','H']
+        #     for y in ['1','2','3','4','5','6','7','8','9','10','11','12']]
+        #get all possible fields related to WellReading
 
+        #previous...
+        self.defaultsdict=defaultsdict
+        self.plateid=uuid.uuid4().hex
+        self.defaultsdict['plateid']=self.plateid
+        self.active_wids=None
+        self.welldict={}#dictionary with wellreadings values
+        self.exceldf=None#df pulled straight from excel sheet
+        #go ahead and set up welldatadf here...
+        self.welldatadf=None
+    def read_excel_sheet(self,ifpath,shname):
+        """
+        Reads excel sheet into the class
+
+        Arguments:
+            ifpath: full path and filename of excel file
+            shname: sheet name in excel file 
+        
         Keyword arguments:
-        prows: list of row letters in the list (default ['A'-'H'])
-        pcols: list of integer column values (default [1-12])
+            kwargs to pass.... (needs implementation) 
+        """
+        self.exceldf=read_tecan_excel(ifpath,shname)
+        #automatically create if create_layout has already run through
+        if len(self.welldict)>0:
+            self.make_welldatadf()
+
+    def create_layout(self,active_rows=['A','B','C','D','E','F','G','H'],
+                      active_cols=[1,2,3,4,5,6,7,8,9,10,11,12],
+                      skipwells=[],repeat_measurements=[],itermethod='by_row',
+                      enames=None,econcs=None,snames=None,sconcs=None,standardnames=None,standardconcs=None,
+                      rxnphs=None,rxntemps=None,rxntimes=None,buffernames=None,
+                      predvlp_dlnfactors=None,postdvlp_dlnfactors=None):
+        """
+        Keyword arguments:
+        active_rows: list of row letters in the list (default ['A'-'H'])
+        active_cols: list of integer column values (default [1-12])
         skipwells: list of str rowcol values to not include (eg. ['A11','C7']) (default [])
         itermethod: direction/rule to iterate through rows/cols in parallel with passing over list
                     (default 'byrow_wrap' = 'A1','A2',...,'B1','B2',...'H12')
@@ -129,41 +185,7 @@ class TecanPlate:
         predvlp_dlnfactors: list of dilution factor before develop step (default = None)
         postdvlp_dlnfactors: list of dilution factor post develop step (default = None)
         """
-        #wellids=[x+y for x in ['A','B','C','D','E','F','G','H']
-        #     for y in ['1','2','3','4','5','6','7','8','9','10','11','12']]
-        #get all possible fields related to WellReading
-
-        #previous...
-        self.all_settings=[dataclasses.fields(x) for x in
-                   [WellReading,WellRxnReading,WellEnzymeSubstrateReading,
-                    WellEnzymeControl,WellSubstrateControl,WellStandard]]
-        self.defaultsdict={x:None for x in self.all_settings}
-
-        #new...        
-#        self.defaultsdict={x.name:x.default for x in dataclasses.fields(y) for y in 
-#                            [WellReading,WellRxnReading,WellEnzymeSubstrateReading,
-#                            WellEnzymeControl,WellSubstrateControl,WellStandard]}
-#        self.all_settings=self.defaultsdict.keys()
-
-        self.defaultsdict.update(defaultsdict)
-        self.plateid=uuid.uuid4().hex
-        self.active_wids=None
-        self.welldict={}#dictionary with wellreadings values
-        self.exceldf=None#df pulled straight from excel sheet
-        #go ahead and set up welldatadf here...
-        self.welldatadf=pd.DataFrame(columns=list(set(self.all_settings)))
-    def read_excel_sheet(self,ifpath,shname):
-        self.exceldf=read_tecan_excel(ifpath,shname)
-        #automatically create if create_layout has already run through
-        if len(self.welldict)>0:
-            self.make_welldatadf()
-
-    def create_layout(self,active_rows=['A','B','C','D','E','F','G','H'],
-                      active_cols=[1,2,3,4,5,6,7,8,9,10,11,12],
-                      skipwells=[],repeat_measurements=[],itermethod='by_row',
-                      enames=None,econcs=None,snames=None,sconcs=None,standardnames=None,standardconcs=None,
-                      rxnphs=None,rxntemps=None,rxntimes=None,buffernames=None,
-                      predvlp_dlnfactors=None,postdvlp_dlnfactors=None):
+ 
         if itermethod not in ['by_row','by_column']:
             raise NotImplementedError('Only byrow_wrap and bycol_wrap iterations across plate are implemneted')
         if itermethod=='by_row':
@@ -172,79 +194,46 @@ class TecanPlate:
             allwids_=[''.join(x[::-1]) for x in itertools.product([str(x) for x in active_cols],active_rows)]
         self.active_wids=[x for x in allwids_ if x not in skipwells]
         #now let's check
-        for x in [enames,econcs,snames,sconcs,standardnames,standardnames,standardconcs,
-                  rxnphs,rxntemps,rxntimes,buffernames,predvlp_dlnfactors,postdvlp_dlnfactors]:
-            if x is not None:
-                assert (len(x)==len(self.active_wids)), \
-                   "length of a varied condition (enames,rxnphs,etc) does not match # of active wells"
+        varied_dict={'ename':enames,'econc':econcs,'sname':snames,'sconc':sconcs,
+                     'standardname':standardnames,'standardconc':standardconcs,
+                     'rxnph':rxnphs,'rxntemp':rxntemps,'rxntime':rxntimes,'buffername':buffernames,
+                     'predvlp_dlnfactor':predvlp_dlnfactors,'postdvlp_dlnfactor':postdvlp_dlnfactors}
+        for ckey in list(varied_dict.keys()):
+            if varied_dict[ckey] is None:
+                del varied_dict[ckey]
+            else:
+                assert (len(varied_dict[ckey])==len(self.active_wids)), \
+                   f"length of a varied condition {ckey} does not match # of active wells"
+
         #assign src_vesselids
-        svrepsdict={}
+        rvrepsdict={}
         if len(repeat_measurements)>0:
             for rmwellids in repeat_measurements:
-                newsvid=uuid.uuid4().hex
-                svrepsdict.update({x:newsvid for x in rmwellids})
+                newrvid=uuid.uuid4().hex
+                rvrepsdict.update({x:newrvid for x in rmwellids})
     #iterate through wids_, build a WellReading for each--
     #use defaultsdict but override with one of optional list arguments if present
     #build a well_dict containing all values for each well, then use it to build WellReading object
         for widx,wid in enumerate(self.active_wids):
-            well_dict=self.defaultsdict.copy()
-            well_dict['wellid']=wid
-            if wid in svrepsdict.keys():
-                well_dict['src_vesselid']=svrepsdict[wid]
+            well_settings_dict=self.defaultsdict.copy()
+            well_settings_dict['wellid']=wid
+            if wid in rvrepsdict.keys():
+                well_settings_dict['rxnvesselid']=rvrepsdict[wid]
             else:
-                well_dict['src_vesselid']=uuid.uuid4().hex
-            if enames is not None:
-                well_dict['ename']=enames[widx]
-            if econcs is not None:
-                well_dict['econc']=econcs[widx]
-            if snames is not None:
-                well_dict['sname']=snames[widx]
-            if sconcs is not None:
-                well_dict['sconc']=sconcs[widx]
-            if standardconcs is not None:
-                well_dict['standardconc']=standardconcs[widx]
-            if rxnphs is not None:
-                well_dict['rxnph']=rxnphs[widx]
-            if rxntemps is not None:
-                well_dict['rxntemp']=rxntemps[widx]
-            if rxntimes is not None:
-                well_dict['rxntime']=rxntimes[widx]
-            if buffernames is not None:
-                well_dict['buffernames']=buffernames[widx]
-            if predvlp_dlnfactors is not None:
-                well_dict['predvlp_dlnfactor']=predvlp_dlnfactors[widx]
-            if postdvlp_dlnfactors is not None:
-                well_dict['postdvlp_dlnfactor']=postdvlp_dlnfactors[widx]
-            #now determine what type of WellReading subclass this is based on keys in well_dict
-            if well_dict['ename'] is not None and well_dict['sname'] is not None:
-                reading=WellEnzymeSubstrateReading(**{x:well_dict[x] for x in
-                             [y.name for y in dataclasses.fields(WellEnzymeSubstrateReading)]
-                             if x in well_dict.keys()})
-            elif well_dict['ename'] is not None and well_dict['sname'] is None:
-                reading=WellEnzymeControl(**{x:well_dict[x] for x in
-                             [y.name for y in dataclasses.fields(WellEnzymeControl)]
-                             if x in well_dict.keys()})
-            elif well_dict['sname'] is not None and well_dict['ename'] is None:
-                reading=WellSubstrateControl(**{x:well_dict[x] for x in
-                             [y.name for y in dataclasses.fields(WellSubstrateControl)]
-                             if x in well_dict.keys()})
-            else:
-                if well_dict['standardconc'] is not None:
-                    reading=WellStandard(**{x:well_dict[x] for x in
-                             [y.name for y in dataclasses.fields(WellStandard)]
-                             if x in well_dict.keys()})
-                else:
-                    print('well not classified')
-            self.welldict[wid]=TecanWell(reading)
-            #automatically create if read_excel_sheet has already run through
-            if self.exceldf is not None:
-                self.make_welldatadf()
+                well_settings_dict['rxnvesselid']=uuid.uuid4().hex
+            well_settings_dict.update({ckey:varied_dict[ckey][widx] for ckey in varied_dict})
+            self.welldict[wid]=TecanWell(well_settings_dict)
+           #should add exception handling here
+           #automatically create if read_excel_sheet has already run through
+        if self.exceldf is not None:
+            self.make_welldatadf()
 
     def make_welldatadf(self):
         assert (self.active_wids is not None), 'plate layout not yet created (call .create_layout())'
         assert (self.welldict[x] is not None for x in self.active_wids), 'not all active_wids created. (call .create_layout())'
         assert (self.exceldf is not None), 'read in the excel file before extracting df (call .read_excel_sheet())'
         #build temporary dataframe from self.welldict then add to target dataframe
+        self.welldatadf=pd.DataFrame(columns=[x.name for x in dataclasses.fields(WellReading)])
         dfrows_=[]
         for wid in self.active_wids:
             dict2add=dataclasses.asdict(self.welldict[wid].wellreading)
